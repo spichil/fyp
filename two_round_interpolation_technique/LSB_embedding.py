44d7fe7958db6da7965797b6f4a14f2431b3de4c
Iwyn Joseph <iwyn0001@student.monash.edu>
2024-10-08 21:51:31 +0800

    Revised code for LSB_embedding. Changes made

--- two_round_interpolation_technique/LSB_embedding.py
+++ two_round_interpolation_technique/LSB_embedding.py
@@ -1,10 +1,10 @@
-import random
 from PIL import Image
+import random
 
-def embed_data_paper(encrypted_image_path, output_image_path, secret_data, data_hiding_key, t=1):
+def embed_data_paper_correct(encrypted_image_path, output_image_path, secret_data, data_hiding_key, t=1, group_size=4):
     """
-    Embedding data by flipping the t-th least significant bit of carrier pixels.
-    Follows the two-round embedding process from the paper.
+    Embedding data by flipping the t-th least significant bit of pixels in non-overlapping groups.
+    Each group embeds one bit as per the method described in the paper.
     """
     # Load the encrypted image
     encrypted_image = Image.open(encrypted_image_path).convert('L')
@@ -29,6 +29,14 @@ def embed_data_paper(encrypted_image_path, output_image_path, secret_data, data_
             elif i % 2 == 1 and j % 2 == 0:  # Set B: odd rows, even columns
                 set_b.append((i, j))
     
+    # Split Set A and Set B into non-overlapping groups
+    def create_groups(pixel_set, group_size):
+        groups = [pixel_set[i:i+group_size] for i in range(0, len(pixel_set), group_size)]
+        return groups
+
+    groups_a = create_groups(set_a, group_size)
+    groups_b = create_groups(set_b, group_size)
+
     # Initialize index for the secret bits
     bit_index = 0
     total_bits = len(secret_bits)
@@ -41,26 +49,28 @@ def embed_data_paper(encrypted_image_path, output_image_path, secret_data, data_
         else:
             return pixel_value ^ mask  # Flip the t-th bit
 
-    # Embed data in Set A first
-    for pixel in set_a:
+    # Embed data into pixel groups in Set A
+    for group in groups_a:
         if bit_index < total_bits:
-            # Get the current pixel value
-            pixel_value = pixel_map[pixel]
-            # Modify the t-th LSB of the pixel value based on the secret bit
-            new_pixel_value = flip_t_bit(pixel_value, secret_bits[bit_index], t)
-            # Update the pixel map
-            pixel_map[pixel] = new_pixel_value
+            # Get the secret bit to embed
+            secret_bit = secret_bits[bit_index]
+            # Modify the t-th LSB of all pixels in the group based on the secret bit
+            for pixel in group:
+                pixel_value = pixel_map[pixel]
+                new_pixel_value = flip_t_bit(pixel_value, secret_bit, t)
+                pixel_map[pixel] = new_pixel_value
             bit_index += 1
 
     # If secret data still remains, use Set B for embedding
-    for pixel in set_b:
+    for group in groups_b:
         if bit_index < total_bits:
-            # Get the current pixel value
-            pixel_value = pixel_map[pixel]
-            # Modify the t-th LSB of the pixel value based on the secret bit
-            new_pixel_value = flip_t_bit(pixel_value, secret_bits[bit_index], t)
-            # Update the pixel map
-            pixel_map[pixel] = new_pixel_value
+            # Get the secret bit to embed
+            secret_bit = secret_bits[bit_index]
+            # Modify the t-th LSB of all pixels in the group based on the secret bit
+            for pixel in group:
+                pixel_value = pixel_map[pixel]
+                new_pixel_value = flip_t_bit(pixel_value, secret_bit, t)
+                pixel_map[pixel] = new_pixel_value
             bit_index += 1
 
     if bit_index < total_bits:
@@ -70,10 +80,8 @@ def embed_data_paper(encrypted_image_path, output_image_path, secret_data, data_
     encrypted_image.save(output_image_path, format="tiff")
     print("Data embedding completed and saved as", output_image_path)
     encrypted_image.show()
-    
-    
 
 # Example usage
-secret_message = "This is the secret data."
-data_hiding_key = "example_key" 
-embed_data_paper("encrypted_image2.tiff", "stego_image_paper.tiff", secret_message, data_hiding_key)
+secret_message = "MSD09"
+data_hiding_key = "123"  # Used for randomization, if needed
+embed_data_paper_correct("encrypted_image2.tiff", "stego_image_correct.tiff", secret_message, data_hiding_key)
